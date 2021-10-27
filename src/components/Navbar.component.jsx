7d45fb01f1178dc92f1d26a8dddd9afa752c80a2
newasad <asadhmeed2@gmail.com>
2021-10-27 13:51:18 +0300

    style navbar

--- src/components/Navbar.component.jsx
+++ src/components/Navbar.component.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Img from "../img/logo2.jpg";
 import "./Navbar.style.css";
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="logo">
-        <Link to="/"></Link>
+        <Link to="/">
+          {" "}
+          <img src={Img} alt="bank logo" />
+        </Link>
       </div>
       <div className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
+        <div className="linkDiv">
+          <Link className="link" to="/">
+            Home
+          </Link>
+        </div>
+        <div className="linkDiv">
+          <Link className="link" to="/login">
+            Login
+          </Link>
+        </div>
+        <div className="linkDiv">
+          <Link className="link" to="/register">
+            Register
+          </Link>
+        </div>
       </div>
     </div>
   );
