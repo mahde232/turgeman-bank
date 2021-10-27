c59d1b88c566807c6e11e84c61f79b06b77a648b
mahde232 <49252163+mahde232@users.noreply.github.com>
2021-10-27 15:11:12 +0300

    added routing between components + minor css

--- src/components/Navbar.component.jsx
+++ src/components/Navbar.component.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Img from "../img/logo2.jpg";
 import "./Navbar.style.css";
-const Navbar = () => {
+const Navbar = ({whoIsLoggedIn,logoutCallback}) => {
+
+  const handleLogout = () => {
+    console.log('logout btn clicked');
+    logoutCallback();
+  }
+
   return (
     <div className="navbar">
       <div className="logo">
@@ -12,21 +18,31 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="nav">
-        <div className="linkDiv">
-          <Link className="link" to="/">
-            Home
-          </Link>
-        </div>
-        <div className="linkDiv">
-          <Link className="link" to="/login">
-            Login
-          </Link>
-        </div>
-        <div className="linkDiv">
-          <Link className="link" to="/register">
-            Register
-          </Link>
-        </div>
+        {
+          whoIsLoggedIn ?
+          (<>
+          {console.log(whoIsLoggedIn)}
+            <div style={{color: "white"}}>Hello {whoIsLoggedIn.firstName}</div>
+            <div className="linkDiv" onClick={handleLogout}>Logout</div>
+          </>)
+          : (<>
+            <div className="linkDiv">
+            <Link className="link" to="/">
+              Home
+            </Link>
+            </div>
+            <div className="linkDiv">
+            <Link className="link" to="/login">
+              Login
+            </Link>
+            </div>
+            <div className="linkDiv">
+              <Link className="link" to="/register">
+                Register
+              </Link>
+            </div>
+          </>)
+        }
       </div>
     </div>
   );
